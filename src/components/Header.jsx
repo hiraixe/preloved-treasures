23baf62da975aed557279039a8c3d9476f328d30
Eunice Pandis <efpandis@student.hau.edu.ph>
2026-05-11 20:06:20 +0800

    dsafsdf

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ function Header({ cartCount }) {
     >
       {/* LOGO */}
       <h2 style={{ margin: 0, color: "#3b5b73" }}>
-        Angelica's Preloved Shop 👜
+        Angelica's Preloved Treasures 👜
       </h2>
 
       {/* NAV */}
